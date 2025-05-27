6a9b79f4be40baece051c5d0225d1fd135f2dec5
Aditi547 <23311a05bm@cse.sreenidhi.edu.in>
2025-05-27 10:15:30 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,50 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONObject;
+public class App{
+    public static void main(String[] args){
+        try{
+            String endpoint = "https://jsonplaceholder.typicode.com/users";
+            URL resourceUrl = new URL(endpoint);
+            HttpURLConnection connection=(HttpURLConnection)resourceUrl.openConnection();
+            connection.setRequestMethod("GET");
+            int statusCode=connection.getResponseCode();
+            if(statusCode==HttpURLConnection.HTTP_OK){
+                BufferedReader reader= new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder jsonData= new StringBuilder();
+                while((line=reader.readLine())!=null){
+                    jsonData.append(line);
+                }
+                reader.close();
+                JSONArray userArray= new JSONArray(jsonData.toString());
+                for(int i=0;i<userArray.length();i++){
+                    JSONObject userObj=userArray.getJSONObject(i);
+                    System.out.println("Name:"+userObj.getString("name"));
+                    System.out.println("Email:"+userObj.getString("email"));
+                    System.out.println("City:"+userObj.getJSONObject("address").getString("city"));
+                    System.out.println("-----------");
+                }
+            }
+            else{
+                System.out.println("GETrequest failed. Response Code:"+statusCode);
+            }
+
+            
+                }
+                catch(Exception exception){
+                    exception.printStackTrace();
+                }
+            }
+        }
+    
+
